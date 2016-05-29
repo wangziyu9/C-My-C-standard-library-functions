1005b1ef70d500112d9cfd90ae5fc239ff5fe260
wangziyu9 <k1wangziyu@gmail.com>
2016-05-29 10:27:03 +0800

    Delete My-isalnum.c

--- My-ctype/My-isalnum.c
+++
@@ -1,9 +0,0 @@
-int MYisalnum(int ch);
-
-int MYisalnum(int ch)   //if is alphanumeric, return 1,or return 0
-{
-    if ((ch >= 16 && ch <= 25) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
-        return 1;
-    else
-        return 0;
-}
