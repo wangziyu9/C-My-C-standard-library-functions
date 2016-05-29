bb834e4a029de23dcaf221685fc47e711d262631
wangziyu9 <k1wangziyu@gmail.com>
2016-05-29 10:27:12 +0800

    Delete My-isalpha.c

--- My-ctype/My-isalpha.c
+++
@@ -1,9 +0,0 @@
-int MYisalpha(int ch);
-
-int MYisalnum(int ch)   //if is alphanumeric, return 1,or return 0
-{
-    if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
-        return 1;
-    else
-        return 0;
-}
