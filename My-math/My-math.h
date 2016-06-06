725e42faedc90755178525703ed056a9053d176f
wangziyu9 <k1wangziyu@gmail.com>
2016-06-06 23:19:22 +0800

    Update My-math.h

--- My-math/My-math.h
+++ My-math/My-math.h
@@ -1,1 +1,31 @@
+#pragma once
 
+int Myabs(signed int c)
+{
+    if (c < 0)
+        return ~(--c);
+    return c;
+}
+
+double Myfabs(double f)
+
+{
+
+    *(((int *)& f) + 1) &= 0x7FFFFFFF;
+
+    return f;
+}
+
+double Mysqrt(double f)
+{
+    double x = f;
+    double y = 0;
+    while (1)
+    {
+        y = x * x - f;
+        if (fabs(y) < 0.0000001)
+            break;
+        x = x - y / (2 * x);
+    }
+    return x;
+}
